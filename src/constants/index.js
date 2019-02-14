aefbc6b671ecbf68b9b283b33da3dc3a04c19322
Zayn Jarvis <ZaynJarvis@gmail.com>
2019-02-15 00:59:25 +0800

    firebase config refactor

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export const FIREBASE_CONFIG = {
+  apiKey: process.env.REACT_APP_FIREBASE_TOKEN,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+};
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,17 +2,10 @@ import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import { FirebaseRepository } from 'hackoss';
+import { FIREBASE_CONFIG } from '../constants';
 
 const FirebaseContext = React.createContext(null);
 
-const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_TOKEN || '',
-  authDomain: 'hackoss-admin.firebaseapp.com',
-  databaseURL: 'https://hackoss-admin.firebaseio.com',
-  projectId: 'hackoss-admin',
-  storageBucket: 'hackoss-admin.appspot.com',
-  messagingSenderId: '841474314837',
-};
 class FirebaseAppBase extends FirebaseRepository {
   constructor(configuration) {
     super(configuration);
@@ -30,7 +23,6 @@ const withFirebase = Component => props => (
     {fb => <Component {...props} firebase={fb} />}
   </FirebaseContext.Consumer>
 );
-const FirebaseApp = Object.freeze(new FirebaseAppBase(config));
+const FirebaseApp = Object.freeze(new FirebaseAppBase(FIREBASE_CONFIG));
 
-export default firebase;
 export { FirebaseContext, FirebaseApp, withFirebase };
